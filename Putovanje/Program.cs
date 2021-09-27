3f0d780f2aa04975efddca41b5ed5993d20b0a1e
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-09-27 11:49:39 +0200

    Add files via upload

---
+++ Putovanje/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Putovanje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double v, s, t;
+            v = double.Parse(Console.ReadLine());
+            s = double.Parse(Console.ReadLine());
+
+            t = s / v;
+
+            Console.WriteLine(Math.Round(t,2));
+        }
+    }
+}
